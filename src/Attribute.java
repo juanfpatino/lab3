e94b8ee243e1c17daaad07a228e2d4c8196f69fe
Juan Patino <juan9patino9@gmail.com>
2022-04-06 17:01:01 -0400

    another attribute

--- src/Attribute.java
+++ src/Attribute.java
@@ -16,6 +16,7 @@ public class Attribute { //takes in a word, determines if it matches a certain a
         return switch (id) {
             case 1 -> isEnglishFunctionWord(words);
             case 2 -> weirdConsonants(words);
+            case 3 -> isDutchFunctionWord(words);
             default -> false;
         };
 
@@ -25,25 +26,47 @@ public class Attribute { //takes in a word, determines if it matches a certain a
     private boolean isEnglishFunctionWord(String[] words){
 
         String[] fWords = new String[]{"the", "a", "it", "he", "him", "she", "her",
-        "his", "hers", "in", "under", "towards", "before", "of", "for", "and", "but", "if", "then", "well",
+        "his", "hers", "under", "towards", "before", "of", "for", "and", "but", "if", "then", "well",
         "however", "thus", "would", "could", "should", "up", "on", "down", "oh", "ah", "eh", "yes", "no", "okay"};
 
         for (String s: fWords
              ) {
 
-            if(Arrays.asList(words).contains(s))return true;
+            String sBeginningOfSentence = Character.toUpperCase(s.charAt(0)) + s.substring(1); //i.e. hello -> Hello
+
+            if(Arrays.asList(words).contains(s) || Arrays.asList(words).contains(sBeginningOfSentence))return true;//english
 
         }
 
-        return false;
+        return false;//dutch
 
     }
 
-    //second attribute. consonants out weird order?
+    //third attribute. function word?
+    private boolean isDutchFunctionWord(String[] words){
+
+        String[] fWords = new String[]{"de", "een", "het", "hij", "hem", "zij", "haar",
+                "zijn", "de hare", "onder", "richting", "voordat", "van", "voor", "en", "maar", "als", "dan", "goed",
+                "echter", "dus", "zou", "zou kunnen", "zou moeten", "omhoog", "aan", "omlaag", "ja", "nee", "oke"};
+
+        for (String s: fWords
+        ) {
+
+            String sBeginningOfSentence = Character.toUpperCase(s.charAt(0)) + s.substring(1); //i.e. haar -> Haar
+
+            if(Arrays.asList(words).contains(s) || Arrays.asList(words).contains(sBeginningOfSentence))return false;//dutch
+
+
+        }
+
+        return true;
+
+    }
+
+    //second attribute. consonants follow dutch patterns?
     private boolean weirdConsonants(String[] words){
 
-        String[] fWords = new String[]{"wd", "tg", "fb", "kt", "jn", "rz", "jv", "fd", };
-
+        String[] fWords = new String[]{"wd", "tg", "fb", "kt", "jn", "rz", "jv", "fd", "jk", "jl", "gv", "kk", "gt", "fv", };
 
         for (String s: fWords
         ) {
